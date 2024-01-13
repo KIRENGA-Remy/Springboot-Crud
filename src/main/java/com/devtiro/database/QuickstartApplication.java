4b91b47a80e6ba0e375c433f34b18175ab655363
Remy_Claudien <gitoliremy.globalsimplilearn.net>
2024-01-13 12:18:12 +0200

    first commit

---
+++ src/main/java/com/devtiro/database/QuickstartApplication.java
@@ -0,0 +1,28 @@
+package com.devtiro.database;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+@SpringBootApplication
+@Log
+
+public class QuickstartApplication implements CommandLineRunner {
+	private final DataSource dataSource;
+
+	public QuickstartApplication(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public static void main(String[] args){
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+	public void run(final String... args){
+		log.info(" GITOLI Remy Claudien "+ dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
+	}
+}
---
+++ src/main/java/com/devtiro/database/service/PersonService.java
@@ -0,0 +1,36 @@
+package com.devtiro.database.service;
+
+import com.devtiro.database.dao.PersonDao;
+import com.devtiro.database.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+@Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person){
+        return personDao.insertPerson(person);
+    }
+    public List<Person> getAllPeople(){
+    return personDao.selectPeople();
+    }
+    public Optional<Person> getPersonById(UUID id){
+    return personDao.selectPersonById(id);
+    }
+    public int deletePerson(UUID id){
+    return personDao.deletePersonById(id);
+    }
+    public int updatePerson(UUID id, Person newPerson){
+    return personDao.updatePersonById(id, newPerson);
+    }
+}
